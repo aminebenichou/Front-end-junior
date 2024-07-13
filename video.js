b33995b7f1c130d1ef886c405e6832e46fc7bf49
Benichou Mohammed El Amine <31164395+aminebenichou@users.noreply.github.com>
2024-07-13 15:41:04 +0100

    video

---
+++ video.js
@@ -0,0 +1,54 @@
+function createSuggestions(data) {
+    const suggestion = document.getElementsByClassName("suggestion")[0]
+
+    const video_card = document.createElement("div")
+    const info = document.createElement("div")
+    const thumbnail = document.createElement("img")
+    const title = document.createElement("span")
+    const channel = document.createElement("span")
+    const views = document.createElement("span")
+    
+    video_card.classList.add("video-card")
+    thumbnail.classList.add("thumbnail")
+    title.classList.add("title")
+    channel.classList.add("channel")
+    views.classList.add("views")
+    info.classList.add("info")
+
+    thumbnail.src = data.snippet.thumbnails.default.url
+    title.innerHTML = data.snippet.title
+    channel.innerHTML = data.snippet.channel
+    // views.innerHTML = data.snippet.thumbnails.default.url
+    
+    video_card.appendChild(thumbnail)
+    video_card.appendChild(info)
+    info.appendChild(title)
+    info.appendChild(channel)
+    info.appendChild(views)
+    
+    suggestion.appendChild(video_card)
+}
+
+async function getVideos() {
+    const url = 'https://youtube-v31.p.rapidapi.com/search?relatedToVideoId=rZNfLzCFf4A&ab&part=id%2Csnippet&type=video&maxResults=50';
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'd6f2791a72msha295b982405ced2p113a58jsn1c774b87e870',
+            'x-rapidapi-host': 'youtube-v31.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result.items);
+        return result.items
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getVideos().then((data)=> data.forEach(video => {
+    createSuggestions(video)
+}))
